cbf0bc26c1753a076652527a6107ba961152ef76
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-06 12:12:32 +0300

    Add FacilitiesSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
 import TrainersSection from '../components/TrainersSection';
+import FacilitiesSection from '../components/FacilitiesSection';
 
 export default function HomePage() {
   return (
@@ -17,6 +18,7 @@ export default function HomePage() {
     <AboutSection />
     <ServicesSection />
     <TrainersSection />
+    <FacilitiesSection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
